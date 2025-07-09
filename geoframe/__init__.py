c067d9fc1cee9f95354c622ee096e038b06830c3
Jayden Rasband <66230914+jrasband-dev@users.noreply.github.com>
2025-07-09 00:55:38 -0600

    Updated __init__.py

--- geoframe/__init__.py
+++ geoframe/__init__.py
@@ -1,7 +1,6 @@
 from .geojson import *
 from .geoarrow import *
 from .geoparquet import *
-from .kml import *
 from .io import *
 
 
@@ -9,8 +8,6 @@ __all__ = [
     'GeoJSON'
     ,'GeoArrow'
     ,'GeoParquet'
-    ,'KML'
     ,'read_geojson'
-    ,'read_kml'
     ,'read_parquet'
 ]
